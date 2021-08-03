f30a9e78c36e132f00de5429cedd770444dedad1
SuYongdeng <suyd@mingdutech.com>
2021-08-03 09:07:02 +0800

    初始化项目

---
+++ src/main/java/com/example/satokendemo/SaTokenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.satokendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaTokenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaTokenDemoApplication.class, args);
+	}
+
+}
