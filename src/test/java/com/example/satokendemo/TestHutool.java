2a762d23201b821bbef2b0a7f1ecf6864a1d0d7c
SuYongdeng <suyd@mingdutech.com>
2021-08-04 17:11:54 +0800

    密码校验

---
+++ src/test/java/com/example/satokendemo/TestHutool.java
@@ -0,0 +1,35 @@
+package com.example.satokendemo;
+
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.crypto.symmetric.SymmetricAlgorithm;
+import cn.hutool.crypto.symmetric.SymmetricCrypto;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.DigestUtils;
+
+public class TestHutool {
+
+    @Test
+    void testSecureUtil() {
+        String content = "test中文";
+        //随机生成密钥
+        byte[] key = SecureUtil.generateKey(SymmetricAlgorithm.AES.getValue()).getEncoded();
+        //构建
+        SymmetricCrypto aes = new SymmetricCrypto(SymmetricAlgorithm.AES, key);
+        //加密
+        byte[] encrypt = aes.encrypt(content);
+        //解密
+        byte[] decrypt = aes.decrypt(encrypt);
+        //加密为16进制表示
+        String encryptHex = aes.encryptHex(content);
+        //解密为字符串
+        String decryptStr = aes.decryptStr(encryptHex);
+        System.out.println(decryptStr);
+    }
+
+    @Test
+    void test(){
+        String pwd = "123456";
+        String md5 = DigestUtils.md5DigestAsHex(pwd.getBytes());
+        System.out.println(md5);
+    }
+}
